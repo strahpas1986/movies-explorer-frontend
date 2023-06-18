e7c6ace96652d16673cab5fb815a9cdccfd30e02
strahpas <strahpas1986@yandex.ru>
2023-06-18 17:41:34 +0300

    сделать кнопку лайков

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -36,17 +36,40 @@ function MoviesCard({ card, isLiked, onCardLike }) {
             aria-label="Добавить в сохранённые фильмы"
             onClick={onCardLike}
           >
-            <svg
+            {/* <svg
               className={`movies-card__btn-like-img ${
                 isLiked ? "movies-card__btn-like-img_active" : ""
               }`}
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill="#fff"
-                d="M7.273 0C6.273 0 5.545.523 5 1.09 4.455.567 3.727 0 2.727 0 1.137 0 0 1.264 0 2.833c0 .785.318 1.482.91 1.962L5 8.5l4.09-3.705c.546-.523.91-1.177.91-1.962C10 1.264 8.864 0 7.273 0Z"
+                fill="#F4F4F4"
+                d="M14 17C17.3137 17 20 14.3137 20 11C20 7.68629 17.3137 5 14 5C10.6863 5 8 7.68629 8 11C8 14.3137 10.6863 17 14 17ZM14 19C18.4183 19 22 15.4183 22 11C22 6.58172 18.4183 3 14 3C9.58172 3 6 6.58172 6 11C6 15.4183 9.58172 19 14 19Z"
               />
+
+            </svg> */}
+            <svg
+              className={`movies-card__btn-like-img ${
+                isLiked ? "movies-card__btn-like-img_active" : ""
+              }`}
+              width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g filter="url(#filter0_d_35657_5470)">
+              <rect x="6" y="3" width="16" height="16" rx="8" fill="#F4F4F4"/>
+              </g>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M14 17C17.3137 17 20 14.3137 20 11C20 7.68629 17.3137 5 14 5C10.6863 5 8 7.68629 8 11C8 14.3137 10.6863 17 14 17ZM14 19C18.4183 19 22 15.4183 22 11C22 6.58172 18.4183 3 14 3C9.58172 3 6 6.58172 6 11C6 15.4183 9.58172 19 14 19Z" fill="white"/>
+              <defs>
+              <filter id="filter0_d_35657_5470" x="0" y="0" width="28" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+              <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+              <feOffset dy="3"/>
+              <feGaussianBlur stdDeviation="3"/>
+              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.09 0"/>
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_35657_5470"/>
+              <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_35657_5470" result="shape"/>
+              </filter>
+              </defs>
             </svg>
+
           </button>
         ) : (
           <button
