573afc59d353155452372f3d553e214e55d887ce
strahpas <strahpas1986@yandex.ru>
2023-07-11 21:38:41 +0300

    исправить порты

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const ESC_KEY = "Escape";
 
 export const MOVIESAPI_URL = "https://api.nomoreparties.co";
-export const MAINAPI_URL = "https://api.movies-strahpas.nomoredomains.rocks"; // для тестирования на локальном ПК, после заменить на Api удаленного сервера
+export const MAINAPI_URL = "https://api.movies-strahpas.nomoredomains.rocks";
 // export const MAINAPI_URL = "http://localhost:3002";
 
 export const USER_NAME_REG_EXP = "^[A-Za-zА-Яа-яЁё\\-\\s]+$";
